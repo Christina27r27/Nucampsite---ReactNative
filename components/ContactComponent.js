fa300dc9f94591389fe640171cc14d60608dee55
Chris <christinarowady@outlook.com>
2020-04-27 15:13:14 -0700

    Icons, Favorites and Comments

---
+++ components/ContactComponent.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+class Contact extends Component{
+
+    static navigationOptions = {
+        title: 'Contact US'
+    }
+
+    render(){
+        return(
+            <ScrollView>
+                <Card
+                    title="ContactInformation"
+                    wrapperStyle={{margin: 20}}
+                >                  
+                    <Text style={{marginBottom:10}}>
+                        1 Nucamp Way
+                    </Text>
+                    <Text>
+                        Seattle, WA 98001
+                    </Text>
+                    <Text>
+                        U.S.A.
+                    </Text>
+                    <Text>
+                        Phone: 1-206-555-1234
+                    </Text>
+                    <Text>
+                        Email: campsites@nucamp.co
+                    </Text>
+                </Card>
+            </ScrollView>
+        )
+    }
+}
+
+export default Contact;
