d9db774beffc22461bcfa5bf1b7c36f83b497aaa
Chris <christinarowady@outlook.com>
2020-04-29 20:41:37 -0700

    Adding Favorites and Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -8,6 +8,19 @@ import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-nativ
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
 import {Icon} from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions,
+    fetchPartners } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
+
+   
+    const mapDispatchToProps = {
+        fetchCampsites,
+        fetchComments,
+        fetchPromotions,
+        fetchPartners
+    };
+    
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -121,6 +134,32 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+
+            headerLeft: <Icon 
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            
+            />
+
+        })
+    }
+);
+
 
 const CustomDrawerContentComponent = props =>(
     <ScrollView>
@@ -171,6 +210,23 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
+
+        Reservation: { 
+            screen: ReservationNavigator, 
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+
+                )
+            }
+        },
+
         About: {
             screen: AboutNavigator,
             navigationOptions: {
@@ -211,6 +267,12 @@ const MainNavigator = createDrawerNavigator(
 
 class Main extends Component {
    
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
     render() {
         return  <View style={{
                     flex: 1,
@@ -251,4 +313,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
